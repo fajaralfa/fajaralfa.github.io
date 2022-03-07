e776e988665b95af55bbe38375c41d1228b7d361
fajaralfa <flyerku@gmail.com>
2022-03-07 14:59:32 +0700

    pindahan

---
+++ script/raraban.js
@@ -0,0 +1,8 @@
+for(let i=1;i<11;i++){
+  document.write('raraban '+i+'<br><br>')
+  for(let a=1;a<11;a++){
+    document.write(i+' * '+a+' = '+a*i+'<br>')
+    console.log(i+' * '+a+' = '+a*i+'\n')
+  }
+  document.write('________________<br><br>')
+}
