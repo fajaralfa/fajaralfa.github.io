504ee24b9a41a6e9cc209dea504aa27e71feab45
fajaralfa <101091656+fajaralfa@users.noreply.github.com>
2024-07-06 22:03:56 +0700

    + darkmode

--- themes/paper/tailwind.config.js
+++ themes/paper/tailwind.config.js
@@ -3,6 +3,7 @@ import typography from '@tailwindcss/typography'
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: [`${__dirname}/layout/**/*.ejs`],
+  darkMode: 'selector',
   theme: {
     extend: {},
   },
