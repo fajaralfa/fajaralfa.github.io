d0ce3db745b5ac3dc6d5c30216534c8c069fca32
fajaralfa <101091656+fajaralfa@users.noreply.github.com>
2024-07-08 00:41:15 +0700

    + postcss renderer script

---
+++ scripts/postcss.js
@@ -0,0 +1,15 @@
+const postcss = require('postcss')
+const postcssrc = require('postcss-load-config')
+
+/**
+ * 
+ * @param {import('hexo/dist/extend/renderer').StoreFunctionData} data 
+ * @returns 
+ */
+export function renderer(data) {
+    return postcssrc()
+        .then(({ plugins, options }) => postcss(plugins).process(data.text, options))
+        .then(result => result.css)
+}
+
+hexo.extend.renderer.register('css', 'css', renderer)
