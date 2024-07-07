165420ffa59dd31a642988151fd8af727e3a413e
fajaralfa <101091656+fajaralfa@users.noreply.github.com>
2024-07-08 00:42:59 +0700

    remove export keyword

--- scripts/postcss.js
+++ scripts/postcss.js
@@ -6,7 +6,7 @@ const postcssrc = require('postcss-load-config')
  * @param {import('hexo/dist/extend/renderer').StoreFunctionData} data 
  * @returns 
  */
-export function renderer(data) {
+function renderer(data) {
     return postcssrc()
         .then(({ plugins, options }) => postcss(plugins).process(data.text, options))
         .then(result => result.css)
