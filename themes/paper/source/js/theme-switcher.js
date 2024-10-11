f8c00971ba6117881812c9f5507e13f030e136ab
fajaralfa <fajaralfrzi@gmail.com>
2024-10-11 14:30:36 +0700

    fix theme switcher button styling cuz tailwind

--- themes/paper/source/js/theme-switcher.js
+++ themes/paper/source/js/theme-switcher.js
@@ -1,7 +1,7 @@
 (() => {
     const themeSwitcher = document.createElement('button')
-    themeSwitcher.classList.value = "fixed right-0 bottom-0 px-2"
-    themeSwitcher.innerHTML = `<img src="/icon/theme-switcher.svg" alt="" srcset="" class="w-10 border-none">`
+    themeSwitcher.id = 'theme-switcher'
+    themeSwitcher.innerHTML = `<img src="/icon/theme-switcher.svg" alt="" srcset="">`
     document.body.append(themeSwitcher)
 
     themeSwitcher.addEventListener('click', () => {
