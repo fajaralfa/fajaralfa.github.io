6eed08fe40445173da3a46da68fa5589e761a073
fajaralfa <fajaralfrzi@gmail.com>
2025-03-10 15:44:54 +0700

    add Header & update style in error page

--- error.vue
+++ error.vue
@@ -9,14 +9,16 @@ const handleError = () => clearError({ redirect: '/' })
 </script>
 
 <template>
-    <div class="relative text-center h-[100vh]">
-        <div class="absolute top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2 space-y-16">
-            <div>
-                <h2 class="font-bold text-9xl">{{ error?.statusCode }}</h2>
-                <div>{{ error?.message }}</div>
-            </div>
-            <button @click="handleError" class="px-4 py-2 border dark:border-gray-500 rounded hover:bg-gray-500/10">Go
-                Home</button>
+    <Header>
+        <div></div>
+        <ThemeToggler />
+    </Header>
+    <div class="absolute top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2 space-y-16 text-center">
+        <div class="space-y-5">
+            <h2 class="font-bold text-8xl">{{ error?.statusCode }}</h2>
+            <div>{{ error?.message }}</div>
         </div>
+        <button @click="handleError" class="px-4 py-2 border dark:border-gray-500 rounded hover:bg-gray-500/10">Go
+            Home</button>
     </div>
 </template>
