7f780aacaa0881583f90ad3af56d13b6443a2de1
fajaralfa <fajaralfrzi@gmail.com>
2025-03-14 11:29:54 +0700

    rewrite content:create script with consola lib

--- scripts/content:create.ts
+++ scripts/content:create.ts
@@ -1,47 +1,50 @@
 import fs from 'node:fs/promises'
-import { defineCommand, runMain } from 'citty'
+import consola from 'consola'
 
-const main = defineCommand({
-    meta: {
-        name: 'Create Content',
-        version: '0.0.1',
-        description: 'Create a Markdown Content',
-    },
-    args: {
-        title: {
-            type: 'positional',
-            description: 'Content Name',
-            required: true,
-        },
-        category: {
-            type: 'positional',
-            description: 'Content Category (post or page)',
-            default: 'post',
-            required: false,
-        },
-        description: {
-            type: 'positional',
-            description: 'Content Description',
-            default: '',
-            required: false,
-        }
-    },
-    async run({ args }) {
-        const filename = args.title.trim().replace(' ', '_') + '.md'
-        const date = new Date().toLocaleDateString('en-CA')
-        let path = 'content/'
-        if (args.category === 'post') path += 'posts' + '/'
-        else if (args.type === 'page') null
-        const filepath = path + filename
-        const file = await fs.open(filepath, 'wx')
-        const stream = file.createWriteStream()
-        stream.write('---\n')
-        stream.write(`title: ${args.title}\n`)
-        stream.write(`description: ${args.description}\n`)
-        stream.write(`date: ${date}\n`)
-        stream.write('---\n')
-        console.log('Content created:', filepath)
-    },
-})
+async function generate(args: { title: string, category: string, description: string }) {
+    const filename = args.title.trim().replace(' ', '_') + '.md'
+    const date = new Date().toLocaleDateString('en-CA')
+    let path = 'content/'
+    if (args.category === 'post') path += 'posts' + '/'
+    const filepath = path + filename
+    const file = await fs.open(filepath, 'wx')
+    const stream = file.createWriteStream()
+    stream.write('---\n')
+    stream.write(`title: ${args.title}\n`)
+    stream.write(`description: ${args.description}\n`)
+    stream.write(`date: ${date}\n`)
+    stream.write('---\n')
+    consola.box(`Content created: "${filepath}"`)
+}
 
-runMain(main)
+async function main() {
+    try {
+        const category = await consola.prompt(
+            'Category',
+            {
+                type: 'text',
+                default: 'post',
+                placeholder: 'post (default)',
+                cancel: 'reject',
+            })
+        const title = await consola.prompt(
+            'Title',
+            {
+                type: 'text',
+                placeholder: 'She, The One that Shining My Class',
+                cancel: 'reject',
+            })
+        const description = await consola.prompt(
+            'Description',
+            {
+                type: 'text',
+                cancel: 'reject',
+                default: '',
+            })
+        generate({ category, title, description })
+    } catch (err) {
+        consola.warn('Canceled.')
+    }
+}
+
+main()
