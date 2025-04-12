ba8d25774b89f11bc0e14d2969c7c699500713a8
fajaralfa <fajaralfrzi@gmail.com>
2025-04-12 10:19:36 +0700

    add Print component

---
+++ components/global/Print.vue
@@ -0,0 +1,10 @@
+<script setup>
+const print = () => window.print()
+</script>
+<template>
+    <ClientOnly>
+        <button @click="print" class="float-right print:hidden underline">
+            <slot />
+        </button>
+    </ClientOnly>
+</template>
