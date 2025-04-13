b43e1d4f74d8bbaa5ef4f2a77207f8d855ed9512
fajaralfa <fajaralfrzi@gmail.com>
2025-04-13 13:25:00 +0700

    simplify code, and add margin in post.description

--- pages/posts/index.vue
+++ pages/posts/index.vue
@@ -10,16 +10,17 @@ definePageMeta({
 <template>
   <div class="space-y-5">
     <div v-for="post in posts" class="hover:shadow-md hover:dark:shadow-slate-300 transition-shadow px-4 py-2 border dark:border-gray-600">
-      <div class="flex flex-col">
-        <span class="text-sm">{{ dateFormat(post.date) }}</span>
-      </div>
+      <div class="text-sm">{{ dateFormat(post.date) }}</div>
       <div>
-        <NuxtLink :href="post.path" class="font-bold text-xl no-underline hover:underline">{{ post.title }}</NuxtLink>
+        <NuxtLink :href="post.path" class="font-bold text-xl">{{ post.title }}</NuxtLink>
       </div>
-      <div>{{ post.description }}</div>
+      <p class="post-description">{{ post.description }}</p>
     </div>
   </div>
 </template>
 
-<style>
+<style scoped>
+.post-description {
+  @apply my-2
+}
 </style>
