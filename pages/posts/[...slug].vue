e1646157cd7f3f9a333ef868a758e0de7e77b1ff
fajaralfa <fajaralfrzi@gmail.com>
2025-04-13 13:23:31 +0700

    add date in post page

--- pages/posts/[...slug].vue
+++ pages/posts/[...slug].vue
@@ -1,4 +1,6 @@
 <script lang="ts" setup>
+import formatDate from '~/utils/dateFormat'
+
 const route = useRoute()
 const { data: page } = await useAsyncData(route.path, () => {
   return queryCollection('post').path(route.path).first()
@@ -25,6 +27,16 @@ definePageMeta({
 <template>
   <div>
     <h1>{{ page?.title ?? 'Untitled' }}</h1>
+    <div class="date">{{ dateFormat(page?.date) }}</div>
     <ContentRenderer v-if="page" :value="page" />
   </div>
 </template>
+
+<style scoped>
+.date {
+  @apply opacity-60;
+}
+h1 {
+  @apply m-0
+}
+</style>
