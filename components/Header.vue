004b115e0ad8ccb7f8016fce8e18213a1b82ac18
fajaralfa <fajaralfrzi@gmail.com>
2025-04-13 11:03:09 +0700

    add padding in header when screen is small

--- components/Header.vue
+++ components/Header.vue
@@ -1,6 +1,6 @@
 <template>
     <header class="print:hidden">
-        <div class="lg:w-[50rem] flex justify-between items-center h-20 w-full p-2 mx-auto">
+        <div class="px-3 lg:px-0 lg:w-[50rem] flex justify-between items-center h-20 w-full p-2 mx-auto">
             <slot />
         </div>
         <hr class="dark:border-gray-600">
